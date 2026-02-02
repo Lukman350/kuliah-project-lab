c2d5e2bfae32d46586dbfa6575d4b18a78d48801
Lukman350 <nulhakiml725@gmail.com>
2026-02-02 21:08:41 +0700

    Add initial project files and define book and sales record structures

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+const char *BOOK_FILE = "databuku.txt";
+const char *SALES_RECORD_FILE = "history.txt";
+
+FILE *bookFile = NULL;
+FILE *salesRecordFile = NULL;
 
 typedef struct
 {
@@ -15,6 +22,16 @@ typedef struct
   int soldCount;
   float totalRevenue;
 } SalesRecord;
+
+void get_all_books(Book **output, size_t *count);
+void get_all_sales_records(SalesRecord **output, size_t *count);
+
+void get_book_by_code(const char *code, Book *output);
+void get_sales_record_by_book_code(const char *bookCode, SalesRecord *output);
+void add_book(const char *name, const char *type, float price);
+void add_sales_record(const char *bookCode, int soldCount);
+void delete_book_by_code(const char *code);
+void delete_sales_record_by_book_code(const char *bookCode);
 
 int main()
 {
